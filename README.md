b81fb35d30cc0a2232dfa9db4c4db283da53ea84
Christian Helle <christian.helle@outlook.com>
2021-10-07 01:37:59 +0200

    Add code snippet to README

--- README.md
+++ README.md
@@ -3,3 +3,42 @@
 
 # pyautodata
 Python library designed to minimize the setup/arrange phase of your unit tests
+
+
+```python
+import unittest
+from typing import List
+from pyautodata import Fixture
+
+class Person:
+    age = 10
+    name = 'test'
+    
+    def get_introduction(self):
+        return f"Hi! My name is {self.name} and I'm {self.age} years old"
+
+    def introduce(self):
+        print(self.get_introduction())
+
+class Staff:
+    def __init__(self):
+        self.people = []
+
+    def add_person(self, person: Person):
+        self.people.append(person)
+
+    def add_people(self, people: List[Person]):
+        for person in people:
+            self.add_person(person)
+
+    def get_head_count(self):
+        return len(self.people)
+
+
+class StaffTests(unittest.TestCase):
+    def test_introduce_everyone(self):
+        people = Fixture.create_many(Person)
+        sut = Fixture.create(Staff)
+        sut.add_people(people)
+        self.assertEqual(len(people), sut.get_head_count())
+```
--- tests/test_create_anonymous_sut.py
+++ tests/test_create_anonymous_sut.py
@@ -1,4 +1,5 @@
 import unittest
+from typing import List
 
 from pyautodata import Fixture
 
@@ -10,6 +11,12 @@ class AutoSutTestCase(unittest.TestCase):
     def test_create_sut_call_function(self):
         self.sut.introduce()
 
+    def test_get_age_returns_non_default_value(self):
+        self.assertNotEqual(Person().get_age(), self.sut.get_age())
+
+    def test_get_name_returns_non_default_value(self):
+        self.assertNotEqual(Person().get_name(), self.sut.get_name())
+
     def test_create_sut_call_function_with_return(self):
         self.assertIsNotNone(self.sut.get_introduction())
 
@@ -18,8 +25,38 @@ class Person:
     age = 10
     name = 'test'
 
+    def get_age(self):
+        return self.age
+
+    def get_name(self):
+        return self.name
+
     def get_introduction(self):
         return f"Hi! My name is {self.name} and I'm {self.age} years old"
 
     def introduce(self):
         print(self.get_introduction())
+
+
+class Staff:
+    def __init__(self):
+        self.people = []
+
+    def add_person(self, person: Person):
+        self.people.append(person)
+
+    def add_people(self, people: List[Person]):
+        for person in people:
+            self.add_person(person)
+
+    def get_head_count(self):
+        return len(self.people)
+
+
+class StaffTests(unittest.TestCase):
+
+    def test_introduce_everyone(self):
+        people = Fixture.create_many(Person)
+        sut = Fixture.create(Staff)
+        sut.add_people(people)
+        self.assertEqual(len(people), sut.get_head_count())
