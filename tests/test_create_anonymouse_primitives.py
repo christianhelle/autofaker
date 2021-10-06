1bb572db4231e879f846ea121de9f693a9dcd360
Christian Helle <christian.helle@outlook.com>
2021-10-06 14:58:36 +0200

    Split up anonymous primitive test cases into several classes

--- tests/test_create_anonymouse_primitives.py
+++ tests/test_create_anonymouse_primitives.py
@@ -1,11 +1,12 @@
 import unittest
 
 import uuid
+from datetime import datetime, date
 
 from pyautodata import Fixture
 
 
-class AnonymousPrimitivesTestCase(unittest.TestCase):
+class AnonymousIntegerTestCase(unittest.TestCase):
     def test_create_int_returns_not_none(self):
         self.assertIsNotNone(Fixture.create(int))
 
@@ -15,6 +16,8 @@ class AnonymousPrimitivesTestCase(unittest.TestCase):
     def test_create_int_returns_not_default(self):
         self.assertNotEqual(Fixture.create(int), 0)
 
+
+class AnonymousStringTestCase(unittest.TestCase):
     def test_create_str_returns_not_none(self):
         self.assertIsNotNone(Fixture.create(str))
 
@@ -23,5 +26,38 @@ class AnonymousPrimitivesTestCase(unittest.TestCase):
 
     def test_create_str_returns_uuid_str(self):
         self.assertIsNotNone(uuid.UUID(Fixture.create(str)))
+
+
+class AnonymousFloatTestCase(unittest.TestCase):
+    def test_create_float_returns_not_none(self):
+        self.assertIsNotNone(Fixture.create(float))
+
+    def test_create_float_returns_not_type(self):
+        self.assertNotIsInstance(Fixture.create(float), type)
+
+    def test_create_float_returns_not_default(self):
+        self.assertNotEqual(Fixture.create(float), float())
+
+
+class AnonymousDatetimeTestCase(unittest.TestCase):
+    def test_create_datetime_returns_not_none(self):
+        self.assertIsNotNone(Fixture.create(datetime))
+
+    def test_create_datetime_returns_not_type(self):
+        self.assertNotIsInstance(Fixture.create(datetime), type)
+
+    def test_create_datetime_returns_datetime(self):
+        self.assertIsInstance(Fixture.create(datetime), datetime)
+
+
+class AnonymousDateTestCase(unittest.TestCase):
+    def test_create_date_returns_not_none(self):
+        self.assertIsNotNone(Fixture.create(date))
+
+    def test_create_date_returns_not_type(self):
+        self.assertNotIsInstance(Fixture.create(date), type)
+
+    def test_create_date_returns_date(self):
+        self.assertIsInstance(Fixture.create(date), date)
 
 
