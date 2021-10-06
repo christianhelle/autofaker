f043aa9c5f2e4a2ec45926dc6c695143fcf9bbad
Christian Helle <christian.helle@outlook.com>
2021-10-07 00:12:27 +0200

    Add tests for create_many using nested classes with lists

---
+++ tests/test_create_many_anonymous_nested_classes_with_lists.py
@@ -0,0 +1,85 @@
+import unittest
+
+from pyautodata import Fixture
+
+
+class SimpleClass:
+    id = 123
+    text = 'test'
+
+
+class NestedWithCollectionClass:
+    id = 123
+    text = 'test'
+    inner = [SimpleClass(), SimpleClass()]
+
+
+class NestedWithNestedCollectionClass:
+    id = 123
+    text = 'test'
+    inner = [NestedWithCollectionClass(), NestedWithCollectionClass()]
+
+
+class AnonymousNestedClassWithCollectionTestCase(unittest.TestCase):
+
+    def test_create_many_nested_class_returns_not_none(self):
+        self.assertIsNotNone(Fixture.create_many(NestedWithCollectionClass))
+
+    def test_create_many_nested_class_returns_list(self):
+        self.assertIsInstance(Fixture.create_many(NestedWithCollectionClass), list)
+
+    def test_create_many_nested_class_returns_instances_with_nested_not_none(self):
+        result = Fixture.create_many(NestedWithCollectionClass)
+        for item in result:
+            self.assertIsNotNone(item.inner)
+
+    def test_create_many_nested_class_returns_instances_with_nested_not_empty(self):
+        result = Fixture.create_many(NestedWithCollectionClass)
+        for item in result:
+            self.assertNotEqual(0, len(item.inner))
+
+    def test_create_many_nested_class_returns_instances_with_new_nested_instance(self):
+        result = Fixture.create_many(NestedWithCollectionClass)
+        for item in result:
+            self.assertNotEqual(item.id, NestedWithCollectionClass().id)
+            for cls in item.inner:
+                self.assertIsNotNone(cls)
+                self.assertNotEqual(cls.id, SimpleClass().id)
+                self.assertNotEqual(cls.text, SimpleClass().text)
+
+
+class AnonymousNestedClassWithNestedCollectionTestCase(unittest.TestCase):
+
+    def test_create_many_nested_class_returns_not_none(self):
+        self.assertIsNotNone(Fixture.create_many(NestedWithCollectionClass))
+
+    def test_create_many_nested_class_returns_list(self):
+        self.assertIsInstance(Fixture.create_many(NestedWithCollectionClass), list)
+
+    def test_create_many_nested_class_returns_instances_with_inner_not_none(self):
+        result = Fixture.create_many(NestedWithNestedCollectionClass)
+        for item in result:
+            self.assertIsNotNone(item.inner)
+
+    def test_create_many_nested_class_returns_instances_with_inner_not_empty(self):
+        result = Fixture.create_many(NestedWithNestedCollectionClass)
+        for item in result:
+            self.assertNotEqual(0, len(item.inner))
+
+    def test_create_many_nested_class_returns_instances_with_nested_inner_not_empty(self):
+        result = Fixture.create_many(NestedWithNestedCollectionClass)
+        for item in result:
+            for cls in item.inner:
+                self.assertNotEqual(0, len(cls.inner))
+
+    def test_create_many_nested_class_returns_instances_with_new_nested_instances(self):
+        result = Fixture.create_many(NestedWithNestedCollectionClass)
+        for root in result:
+            self.assertNotEqual(root.id, NestedWithNestedCollectionClass().id)
+            self.assertNotEqual(root.text, NestedWithNestedCollectionClass().text)
+            for inner in root.inner:
+                self.assertNotEqual(inner.id, NestedWithCollectionClass().id)
+                self.assertNotEqual(inner.text, NestedWithCollectionClass().text)
+                for nested_inner in inner.inner:
+                    self.assertNotEqual(nested_inner.id, SimpleClass().id)
+                    self.assertNotEqual(nested_inner.text, SimpleClass().text)
