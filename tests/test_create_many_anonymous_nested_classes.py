99e2916a79514e70b5c5e4c1e3657b0b96ac05bd
Christian Helle <christian.helle@outlook.com>
2021-10-06 23:58:36 +0200

    Add tests for create_many() using nested classes

---
+++ tests/test_create_many_anonymous_nested_classes.py
@@ -0,0 +1,69 @@
+import unittest
+
+from pyautodata import Fixture
+
+
+class SimpleClass:
+    id = 123
+    text = 'test'
+
+
+class NestedClass:
+    id = 123
+    text = 'test'
+    inner = SimpleClass()
+
+
+class DoubleNestedClass:
+    id = 123
+    text = 'test'
+    inner = NestedClass()
+
+
+class AnonymousNestedClassTestCase(unittest.TestCase):
+
+    def test_create_many_nested_class_returns_not_none(self):
+        self.assertIsNotNone(Fixture.create_many(NestedClass))
+
+    def test_create_many_nested_class_returns_list(self):
+        self.assertIsInstance(Fixture.create_many(NestedClass), list)
+
+    def test_create_many_nested_class_returns_list_with_not_none(self):
+        result = Fixture.create_many(NestedClass)
+        for item in result:
+            self.assertIsNotNone(item.inner)
+
+    def test_create_many_nested_class_returns_instance_with_new_nested_instance(self):
+        result = Fixture.create_many(NestedClass)
+        for item in result:
+            self.assertNotEqual(item.id, NestedClass().id)
+            self.assertNotEqual(item.inner.id, SimpleClass().id)
+            self.assertNotEqual(item.inner.text, SimpleClass().text)
+
+
+class AnonymousDoubleNestedClassTestCase(unittest.TestCase):
+
+    def test_create_many_nested_class_returns_not_none(self):
+        self.assertIsNotNone(Fixture.create_many(DoubleNestedClass))
+
+    def test_create_many_nested_class_returns_instance(self):
+        self.assertIsInstance(Fixture.create_many(DoubleNestedClass), list)
+
+    def test_create_many_nested_class_returns_instance_with_nested_not_none(self):
+        result = Fixture.create_many(DoubleNestedClass)
+        for item in result:
+            self.assertIsNotNone(item)
+            self.assertIsNotNone(item.inner)
+
+    def test_create_many_nested_class_returns_instance_with_new_nested_instance(self):
+        result = Fixture.create_many(DoubleNestedClass)
+        for item in result:
+            self.assertNotEqual(item.id, DoubleNestedClass().id)
+            self.assertNotEqual(item.inner, NestedClass())
+
+    def test_create_many_nested_class_returns_instance_with_new_double_nested_instance(self):
+        result = Fixture.create_many(DoubleNestedClass)
+        for item in result:
+            self.assertNotEqual(item.id, DoubleNestedClass().id)
+            self.assertNotEqual(item.inner.inner.id, SimpleClass().id)
+            self.assertNotEqual(item.inner.inner.text, SimpleClass().text)
