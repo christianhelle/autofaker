c245b356b44f6d726001b870f9e5a9f93fd9fa47
Christian Helle <christian.helle@outlook.com>
2021-10-08 13:21:02 +0200

    Add docstring to Fixture class

--- src/pyautodata/fixture.py
+++ src/pyautodata/fixture.py
@@ -1,5 +1,6 @@
 import typing
 from typing import List
+from pyspark.sql import DataFrame
 
 from pyautodata.generator import TypeDataGenerator
 
@@ -7,10 +8,21 @@ from pyautodata.generator import TypeDataGenerator
 class Fixture:
     @staticmethod
     def create(t):
+        """
+        Creates an anonymous variable of the requested type
+
+        :type t: object
+        """
         return TypeDataGenerator.create(t).generate()
 
     @staticmethod
     def create_many(t, size: int = 3) -> List[typing.Any]:
+        """
+        Creates a list of anonymous variables of the requested type using the specified length (default 3)
+
+        :type size: int
+        :type t: object
+        """
         items = []
         for i in range(size):
             items.append(Fixture.create(t))
