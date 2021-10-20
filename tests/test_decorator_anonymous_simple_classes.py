60c9d89bbbd4569e5ecce5950f269557d7b702ac
Christian Helle <christian.helle@outlook.com>
2021-10-21 00:24:36 +0200

    Add AnonymousSimpleClassViaDecoratorTestCase

---
+++ tests/test_decorator_anonymous_simple_classes.py
@@ -0,0 +1,24 @@
+import unittest
+
+from autofaker import Autodata
+
+
+class SimpleClass:
+    id = 123
+    text = 'test'
+
+
+class AnonymousSimpleClassViaDecoratorTestCase(unittest.TestCase):
+
+    @Autodata.create_argument(SimpleClass)
+    def test_create_simple_class_using_decorator_returns_not_none(self, instance):
+        self.assertIsNotNone(instance)
+
+    @Autodata.create_argument(SimpleClass)
+    def test_create_simple_class_using_decoratorreturns_instance(self, instance):
+        self.assertIsInstance(instance, SimpleClass)
+
+    @Autodata.create_argument(SimpleClass)
+    def test_create_simple_class_returns_instance_with_new_values(self, instance):
+        self.assertNotEqual(instance.id, SimpleClass().id)
+        self.assertNotEqual(instance.text, SimpleClass().text)
