0afebc43bb2bf922027ba2190272abd11ec5908e
Christian Helle <christian.helle@outlook.com>
2021-10-24 23:04:55 +0200

    Update decorator tests

--- tests/test_decorator_anonymous_nested_classes.py
+++ tests/test_decorator_anonymous_nested_classes.py
@@ -5,17 +5,20 @@ from autofaker import Autodata
 
 class SimpleClass:
     id = 123
+    name = 'name'
     text = 'test'
 
 
 class NestedClass:
     id = 123
+    name = 'name'
     text = 'test'
     inner = SimpleClass()
 
 
 class DoubleNestedClass:
     id = 123
+    name = 'name'
     text = 'test'
     inner = NestedClass()
 
@@ -36,6 +39,24 @@ class AnonymousNestedClassViaDecoratorTestCase(unittest.TestCase):
         self.assertNotEqual(instance.inner.id, SimpleClass().id)
         self.assertNotEqual(instance.inner.text, SimpleClass().text)
 
+    @Autodata.create_arguments()
+    def test_create_nested_class_argument_returns_instance_with_new_values(self, instance: NestedClass):
+        self.assertNotEqual(instance.id, NestedClass().id)
+        self.assertNotEqual(instance.name, SimpleClass().name)
+        self.assertNotEqual(instance.text, SimpleClass().text)
+
+    @Autodata.create_anonymous_arguments
+    def test_create_anonymous_nested_class_returns_instance_with_new_values(self, instance: NestedClass):
+        self.assertNotEqual(instance.id, SimpleClass().id)
+        self.assertNotEqual(instance.name, SimpleClass().name)
+        self.assertNotEqual(instance.text, SimpleClass().text)
+
+    @Autodata.create_fake_arguments
+    def test_create_fake_nested_class_returns_instance_with_new_values(self, instance: NestedClass):
+        self.assertNotEqual(instance.id, NestedClass().id)
+        self.assertNotEqual(instance.name, SimpleClass().name)
+        self.assertNotEqual(instance.text, SimpleClass().text)
+
 
 class AnonymousDoubleNestedClassViaDecoratorWithFakesTestCase(unittest.TestCase):
 
@@ -49,6 +70,21 @@ class AnonymousDoubleNestedClassViaDecoratorWithFakesTestCase(unittest.TestCase)
 
     @Autodata.create_arguments(DoubleNestedClass, use_fake_data=True)
     def test_create_double_nested_class_returns_instance_with_new_values(self, instance):
+        self.assertNotEqual(instance.id, DoubleNestedClass().id)
+        self.assertNotEqual(instance.inner, DoubleNestedClass().inner)
+        self.assertNotEqual(instance.inner.inner.id, SimpleClass().id)
+        self.assertNotEqual(instance.inner.inner.text, SimpleClass().text)
+
+    @Autodata.create_arguments(use_fake_data=True)
+    def test_create_double_nested_class_argument_using_decorator_returns_not_none(self, instance: DoubleNestedClass):
+        self.assertIsNotNone(instance)
+
+    @Autodata.create_arguments(use_fake_data=True)
+    def test_create_double_nested_class_argument_using_decorator_returns_instance(self, instance: DoubleNestedClass):
+        self.assertIsInstance(instance, DoubleNestedClass)
+
+    @Autodata.create_fake_arguments
+    def test_create_double_nested_class_argument_returns_instance_with_new_values(self, instance: DoubleNestedClass):
         self.assertNotEqual(instance.id, DoubleNestedClass().id)
         self.assertNotEqual(instance.inner, DoubleNestedClass().inner)
         self.assertNotEqual(instance.inner.inner.id, SimpleClass().id)
--- tests/test_decorator_anonymous_simple_classes.py
+++ tests/test_decorator_anonymous_simple_classes.py
@@ -5,6 +5,7 @@ from autofaker import Autodata
 
 class SimpleClass:
     id = 123
+    name = 'name'
     text = 'test'
 
 
@@ -15,12 +16,31 @@ class AnonymousSimpleClassViaDecoratorTestCase(unittest.TestCase):
         self.assertIsNotNone(instance)
 
     @Autodata.create_arguments(SimpleClass)
-    def test_create_simple_class_using_decoratorreturns_instance(self, instance):
+    def test_create_simple_class_using_decorator_returns_instance(self, instance):
         self.assertIsInstance(instance, SimpleClass)
 
     @Autodata.create_arguments(SimpleClass)
     def test_create_simple_class_returns_instance_with_new_values(self, instance):
         self.assertNotEqual(instance.id, SimpleClass().id)
+        self.assertNotEqual(instance.name, SimpleClass().name)
+        self.assertNotEqual(instance.text, SimpleClass().text)
+
+    @Autodata.create_arguments()
+    def test_create_simple_class_argument_returns_instance_with_new_values(self, instance: SimpleClass):
+        self.assertNotEqual(instance.id, SimpleClass().id)
+        self.assertNotEqual(instance.name, SimpleClass().name)
+        self.assertNotEqual(instance.text, SimpleClass().text)
+
+    @Autodata.create_anonymous_arguments
+    def test_create_anonymous_simple_class_returns_instance_with_new_values(self, instance: SimpleClass):
+        self.assertNotEqual(instance.id, SimpleClass().id)
+        self.assertNotEqual(instance.name, SimpleClass().name)
+        self.assertNotEqual(instance.text, SimpleClass().text)
+
+    @Autodata.create_fake_arguments
+    def test_create_fake_simple_class_returns_instance_with_new_values(self, instance: SimpleClass):
+        self.assertNotEqual(instance.id, SimpleClass().id)
+        self.assertNotEqual(instance.name, SimpleClass().name)
         self.assertNotEqual(instance.text, SimpleClass().text)
 
 
@@ -37,4 +57,16 @@ class AnonymousSimpleClassViaDecoratorWithFakesTestCase(unittest.TestCase):
     @Autodata.create_arguments(SimpleClass, use_fake_data=True)
     def test_create_simple_class_returns_instance_with_new_values(self, instance):
         self.assertNotEqual(instance.id, SimpleClass().id)
+        self.assertNotEqual(instance.text, SimpleClass().text)
+
+    @Autodata.create_arguments(use_fake_data=True)
+    def test_create_simple_class_argument_returns_instance_with_new_values(self, instance: SimpleClass):
+        self.assertNotEqual(instance.id, SimpleClass().id)
+        self.assertNotEqual(instance.name, SimpleClass().name)
+        self.assertNotEqual(instance.text, SimpleClass().text)
+
+    @Autodata.create_fake_arguments
+    def test_create_fake_simple_class_returns_instance_with_new_values(self, instance: SimpleClass):
+        self.assertNotEqual(instance.id, SimpleClass().id)
+        self.assertNotEqual(instance.name, SimpleClass().name)
         self.assertNotEqual(instance.text, SimpleClass().text)
