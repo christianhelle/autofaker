e0d90239123672fac3a2ad9648027b1b3355c85e
Christian Helle <christian.helle@outlook.com>
2021-11-13 22:08:57 +0100

    Add more comprehensive tests for generating data frames

--- tests/test_decorator_anonymous_dataframe_pandas.py
+++ tests/test_decorator_anonymous_dataframe_pandas.py
@@ -6,29 +6,57 @@ import pandas
 from autofaker import autopandas, fakepandas
 
 
-class SimpleClass:
+class SimpleClassA:
     id = -1
     name: 'test'
     text = 'test'
 
 
+class SimpleClassB:
+    def __init__(self, id: int, name: str, text: str):
+        self.text = text
+        self.name = name
+        self.id = id
+
+
+class SimpleClassC:
+    def __init__(self, a: SimpleClassA, b: SimpleClassB):
+        self.b = b
+        self.a = a
+
+
 class AnonymousPandasDataFrameViaDecoratorTests(unittest.TestCase):
 
-    @autopandas(SimpleClass)
+    @autopandas(SimpleClassA)
     def test_create_anonymous_pandas_dataframe_returns_not_none(self, df: pandas.DataFrame):
         self.assertIsNotNone(df)
 
-    @autopandas(SimpleClass, 10)
+    @autopandas(SimpleClassA, 10)
     def test_create_anonymous_pandas_dataframe_with_rowcount_returns_not_empty(self, df: pandas.DataFrame):
         self.assertNotEqual(len(df.index), 0)
 
-    @fakepandas(SimpleClass)
+    @fakepandas(SimpleClassA)
     def test_create_fake_pandas_dataframe_returns_not_none(self, df: pandas.DataFrame):
         self.assertIsNotNone(df)
 
-    @fakepandas(SimpleClass, 10)
+    @fakepandas(SimpleClassA, 10)
     def test_create_fake_pandas_dataframe_with_rowcount_returns_not_empty(self, df: pandas.DataFrame):
         self.assertNotEqual(len(df.index), 0)
+
+    @autopandas(SimpleClassA)
+    def test_can_create_anonymous_pandas_dataframes(self, cls):
+        print(cls)
+        self.assertIsNotNone(cls)
+
+    @autopandas(SimpleClassB)
+    def test_can_create_anonymous_pandas_dataframes_from_class_with_constructor_arguments(self, cls):
+        print(cls)
+        self.assertIsNotNone(cls)
+
+    @autopandas(SimpleClassC)
+    def test_can_create_anonymous_pandas_dataframes_from_class_with_constructor_class_arguments(self, cls):
+        print(cls)
+        self.assertIsNotNone(cls)
 
 
 @dataclass
--- tests/test_decorator_anonymous_dataframe_spark.py
+++ tests/test_decorator_anonymous_dataframe_spark.py
@@ -7,30 +7,58 @@ from pyspark.sql import DataFrame
 from autofaker import autospark, fakespark
 
 
-class SimpleClass:
+class SimpleClassA:
     id = -1
     name = 'test'
     text = 'test'
 
 
+class SimpleClassB:
+    def __init__(self, id: int, name: str, text: str):
+        self.text = text
+        self.name = name
+        self.id = id
+
+
+class SimpleClassC:
+    def __init__(self, a: SimpleClassA, b: SimpleClassB):
+        self.b = b
+        self.a = a
+
+
 @unittest.skipIf(sys.platform.startswith("win") or sys.platform == "darwin", "tests are very slow")
 class AnonymousSparkDataFrameViaDecoratorTests(unittest.TestCase):
 
-    @autospark(SimpleClass)
+    @autospark(SimpleClassA)
     def test_create_anonymous_spark_dataframe_returns_not_none(self, df: DataFrame):
         self.assertIsNotNone(df)
 
-    @autospark(SimpleClass, 10)
+    @autospark(SimpleClassA, 10)
     def test_create_anonymous_spark_dataframe_with_rowcount_returns_not_empty(self, df: DataFrame):
         self.assertNotEqual(df.count(), 0)
 
-    @fakespark(SimpleClass)
+    @fakespark(SimpleClassA)
     def test_create_fake_spark_dataframe_returns_not_none(self, df: DataFrame):
         self.assertIsNotNone(df)
 
-    @fakespark(SimpleClass, 10)
+    @fakespark(SimpleClassA, 10)
     def test_create_fake_spark_dataframe_with_rowcount_returns_not_empty(self, df: DataFrame):
         self.assertNotEqual(df.count(), 0)
+
+    @autospark(SimpleClassA)
+    def test_can_create_anonymous_pandas_dataframes(self, cls):
+        print(cls)
+        self.assertIsNotNone(cls)
+
+    @autospark(SimpleClassB)
+    def test_can_create_anonymous_pandas_dataframes_from_class_with_constructor_arguments(self, cls):
+        print(cls)
+        self.assertIsNotNone(cls)
+
+    @autospark(SimpleClassC)
+    def test_can_create_anonymous_pandas_dataframes_from_class_with_constructor_class_arguments(self, cls):
+        print(cls)
+        self.assertIsNotNone(cls)
 
 
 @dataclass
