95e7bd68a1aa74838f99ddaf7f13bc82e00986c8
Christian Helle <christian.helle@outlook.com>
2021-11-15 16:23:02 +0100

    Fix PandasDataFrameGenerator support for nested data classes

--- src/autofaker/dataframe.py
+++ src/autofaker/dataframe.py
@@ -1,3 +1,5 @@
+import dataclasses
+
 import pandas as pd
 
 from autofaker.attributes import Attributes
@@ -5,26 +7,19 @@ from autofaker.generator import TypeDataGenerator
 
 
 class PandasDataFrameGenerator:
-    """
-    Create a Pandas DataFrame containing anonymous data with the specified number of rows (default 3)
-    """
-
     def __init__(self, t, rows: int = 3, use_fake_data: bool = False):
-        """
-        Create a Pandas DataFrame containing anonymous data with the specified number of rows (default 3)
-
-        :type rows: int
-        :type t: object
-        :type use_fake_data: bool
-        """
-        self.__data = []
+        self.data = []
         for _ in range(rows):
-            self.__data.append(TypeDataGenerator.create(t, use_fake_data=use_fake_data).generate())
+            row = TypeDataGenerator.create(t, use_fake_data=use_fake_data).generate()
+            self.data.append(row)
 
     def generate(self):
-        members = Attributes(self.__data[0]).get_members()
+        members = [
+            attr for attr in dir(self.data[0])
+            if dataclasses.is_dataclass(getattr(self.data[0], attr)) or not callable(getattr(self.data[0], attr)) and not attr.startswith("__")
+        ]
         rows = []
-        for d in self.__data:
+        for d in self.data:
             row = []
             for member in members:
                 row.append(Attributes(d).get_attribute(member))
@@ -33,18 +28,7 @@ class PandasDataFrameGenerator:
 
 
 class SparkDataFrameGenerator:
-    """
-    Create a Spark DataFrame containing anonymous data with the specified number of rows (default 3)
-    """
-
     def __init__(self, t, rows: int = 3, use_fake_data: bool = False):
-        """
-        Create a Pandas DataFrame containing anonymous data with the specified number of rows (default 3)
-
-        :type rows: int
-        :type t: object
-        :type use_fake_data: bool
-        """
         self.use_fake_data = use_fake_data
         self.t = t
         self.rows = rows
