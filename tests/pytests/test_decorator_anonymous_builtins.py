3c196f4c3c7e11b0e04e94e76ddbada35eebb2a9
Christian Helle <christian.helle@outlook.com>
2022-10-03 11:45:57 +0200

    Add PyTests for Anonymous Primitives Via Decorator With Fakes

--- tests/pytests/test_decorator_anonymous_builtins.py
+++ tests/pytests/test_decorator_anonymous_builtins.py
@@ -48,3 +48,49 @@ def test_create_datetime_using_decorator(dt):
 @autodata(date)
 def test_create_date_using_decorator(d):
     assert None != (d)
+
+
+# Anonymous Primitives Via Decorator With Fakes
+
+@autodata(str, use_fake_data=True)
+def test_create_str_using_decorator(text):
+    assert text is not None
+
+@autodata(int, use_fake_data=True)
+def test_create_int_using_decorator(number):
+    assert None != (number)
+
+@autodata(float, use_fake_data=True)
+def test_create_float_using_decorator(number):
+    assert None != (number)
+
+@autodata(bool, use_fake_data=True)
+def test_create_boolean_using_decorator(boolean):
+    assert None != (boolean)
+
+@autodata(complex, use_fake_data=True)
+def test_create_complex_using_decorator(complex_type):
+    assert None != (complex_type)
+
+@autodata(range, use_fake_data=True)
+def test_create_range_using_decorator(r):
+    assert None != (r)
+
+@autodata(bytes, use_fake_data=True)
+def test_create_bytes_using_decorator(b):
+    assert None != (b)
+
+@autodata(bytearray, use_fake_data=True)
+def test_create_bytearray_using_decorator(b):
+    assert None != (b)
+
+@autodata(memoryview, use_fake_data=True)
+def test_create_memoryview_using_decorator(b):
+    assert None != (b)
+
+@autodata(datetime, use_fake_data=True)
+def test_create_datetime_using_decorator(dt):
+    assert None != (dt)
+
+@autodata(date, use_fake_data=True)
+def test_create_date_using_decorator(d):
