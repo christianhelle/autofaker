599c7c00bbb36f411c6591613cc4b100645993c4
Christian Helle <christian.helle@outlook.com>
2024-05-13 15:39:26 +0200

    Remove support for Python 3.7 in build workflows and dependencies

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=["pandas", "faker", "typing_inspect"],
 )
