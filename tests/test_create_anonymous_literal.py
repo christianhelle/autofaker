e6554e7c0457338df2012f3bb127a8194f6f718d
Christian Helle <christian.helle@outlook.com>
2025-05-14 12:49:20 +0200

    Refactor tests to support multiple literal types
    Renamed variables and test cases to differentiate string and number literals. Added new test cases for number literal type and updated the dataclass to include both string and number literals. This enhances test coverage and supports broader use cases.

--- tests/test_create_anonymous_literal.py
+++ tests/test_create_anonymous_literal.py
@@ -4,23 +4,35 @@ from typing import Literal
 
 from autofaker import Autodata
 
-literal = Literal["pending", "approved", "rejected"]
+string_literal = Literal["pending", "approved", "rejected"]
+number_literal = Literal[1, 2, 3]
 
 
-class AnonymousLiteralTypeTestCase(unittest.TestCase):
+class AnonymousStringLiteralTypeTestCase(unittest.TestCase):
     def test_create_literal_returns_not_none(self):
-        data = Autodata.create(literal)
+        data = Autodata.create(string_literal)
         self.assertIsNotNone(data)
 
     def test_create_literal_returns_valid_value(self):
-        data = Autodata.create(literal)
+        data = Autodata.create(string_literal)
         self.assertIn(data, ["pending", "approved", "rejected"])
+
+
+class AnonymousNumberLiteralTypeTestCase(unittest.TestCase):
+    def test_create_literal_returns_not_none(self):
+        data = Autodata.create(number_literal)
+        self.assertIsNotNone(data)
+
+    def test_create_literal_returns_valid_value(self):
+        data = Autodata.create(number_literal)
+        self.assertIn(data, [1, 2, 3])
 
 
 @dataclass
 class DataClassWithLiteral:
     id: int
-    status: literal
+    status: string_literal
+    number: number_literal
 
 
 class AnonymousDataClassWithLiteralTypeTestCase(unittest.TestCase):
@@ -31,7 +43,9 @@ class AnonymousDataClassWithLiteralTypeTestCase(unittest.TestCase):
     def test_create_literal_returns_valid_value(self):
         data = Autodata.create(DataClassWithLiteral)
         self.assertIn(data.status, ["pending", "approved", "rejected"])
+        self.assertIn(data.number, [1, 2, 3])
 
     def test_create_literal_returns_fake_value(self):
         data = Autodata.create(DataClassWithLiteral, use_fake_data=True)
         self.assertIn(data.status, ["pending", "approved", "rejected"])
+        self.assertIn(data.number, [1, 2, 3])
